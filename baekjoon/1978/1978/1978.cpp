cb6feae1b4bafbd8b4be1b0b78c0777c81d71245
sksskstkdghk <wjdtkdghk90@gmail.com>
2022-01-31 03:16:05 +0900

    Study

---
+++ baekjoon/1978/1978/1978.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, result = 0;
+    bool eratos[1001];
+
+    cin >> n;
+
+    for (int i = 0; i <= 1000; i++)
+        eratos[i] = true;
+
+    for (int i = 2; i <= 1000; i++)
+    {
+        if (eratos[i])
+            for (int j = i * i; j <= 1000; j += i)
+                eratos[j] = false;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+
+        if (a < 2)
+            continue;
+
+        if (eratos[a])
+            result++;
+    }
+
+    cout << result;
+}
---
+++ baekjoon/2581/2581/2581.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int min = 0, sum = 0;
+    int a, b;
+    bool eratos[10001] = {};
+
+    cin >> a >> b;
+
+    for (int i = 0; i <= b; i++)
+        eratos[i] = true;
+
+    for (int i = 2; i <= b; i++)
+    {
+        if (eratos[i])
+            for (int j = i * i; j <= b; j += i)
+                eratos[j] = false;
+    }
+
+    for (int i = a; i <= b; i++)
+    {
+        if (i < 2)
+            continue;
+
+        if (eratos[i])
+        {
+            sum += i;
+
+            if (min <= 0)
+                min = i;
+        }
+    }
+
+    if (sum <= 0)
+        cout << -1;
+    else
+        cout << sum << "\n" << min << endl;
+}
